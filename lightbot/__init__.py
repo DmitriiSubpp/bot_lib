cb03f5ec740f545695eea3363fc041374f83542d
Dmitrii Oreshkin <71903279+dmitrii-subb@users.noreply.github.com>
2022-10-26 23:41:47 +0300

    add DEBUG messages

--- lightbot/__init__.py
+++ lightbot/__init__.py
@@ -4,14 +4,20 @@ Just 'from lightbot import bot'.
 """
 
 from ._lightbot import Core
-
-from ._text_handler import text_handler
-from ._voice_handler import voice_handler
+from ._lightbot import InlineKeyboard, ReplyKeyboard
+from ._text_handler import TextHandler
+from ._callback_handler import CallbackHandler
+from ._file_handler import FileHandler
+from ._location_handler import LocationHandler
 
 __version__ = "0.1.0"
-#__all__ = ["bot"]
+
+__all__ = ["bot"]
+
 
 bot = Core(
-    text_handler=text_handler,
-    voice_handler=voice_handler,
+    text_handler=TextHandler(),
+    location_handler=LocationHandler(),
+    file_handler=FileHandler(),
+    callback_handler=CallbackHandler(),
 )
